d06e556dbf2fad674e48cf2fc9d4e509a4002d61
david <david_Alisson60@yahoo.com.br>
2024-12-16 17:49:31 -0300

    14 - Pipe operators do angular

---
+++ src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pipes',
+  imports: [CommonModule],
+  templateUrl: './pipes.component.html',
+  styleUrl: './pipes.component.css'
+})
+export class PipesComponent {
+  someText = "TESTANDO O PIPE OPERATOR"
+  today = new Date();
+}
