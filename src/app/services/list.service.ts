4516ccf31b6d52bc86b73f4e2060b2c7937cd11d
david <david_Alisson60@yahoo.com.br>
2024-12-28 19:31:07 -0300

    20 - Excluindo dados pelo Service

--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -19,8 +19,8 @@ export class ListService {
 
   constructor(private http : HttpClient) {}
 
-  remove(animals: Animal[], animal: Animal) {
-    return animals.filter((a) => animal.name !== a.name);
+  remove(id : Number) {
+    return this.http.delete<Animal>(`${this.apiUrlAnimals}/${id}`);
   }
 
   getAll() : Observable<Animal[]>{
